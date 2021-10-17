6a6f4cfa1f725d8b0ede5758ea346c8dda02dc21
aiguyuk <75844810+aiguyuk@users.noreply.github.com>
2021-10-17 18:07:40 +0100

    Add files via upload

---
+++ src/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import logo from './logo192.png';
+
+
+
+ReactDOM.render( <
+    React.StrictMode >
+    <
+    App / >
+    <
+    /React.StrictMode>,
+    document.getElementById('root')
+);
